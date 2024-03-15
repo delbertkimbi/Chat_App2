77c89f74150d8c936453bef848c25cc6c59643ed
delbert <delbertdrums5@gmail.com>
2024-03-16 00:13:18 +0100

    Added Custom button style

--- chat_app/lib/pages/screens/home.dart
+++ chat_app/lib/pages/screens/home.dart
@@ -9,47 +9,68 @@ class HomePAge extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.fromLTRB(10, 30, 10, 10),
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/image1.png',
-              alignment: Alignment.topCenter,
-              height: 271,
-              width: 262,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 20),
-            Container(
-              height: 90,
-              width: 280,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Image.asset(
+                    'assets/image1.png',
+                    alignment: Alignment.topCenter,
+                    height: 271,
+                    width: 262,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 100,
+                    width: 280,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Text(
+                      'Connect easily with your family and friends over Countries',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black,
+                        //  color: Color.fromARGB(1, 15, 24, 40),
+                        fontFamily: 'Mulish',
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              child: const Text(
-                'Connect with your family and friends over Countries',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
-                  color: Color.fromARGB(1, 15, 24, 40),
-                  fontFamily: 'Mulish',
-                ),
+              const SizedBox(height: 10),
+              const SizedBox(height: 20),
+              const Column(
+                children: [
+                  Text(
+                    "Terms & Privacy Policy",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      // color: Color.fromARGB(1, 15, 24, 40),
+                      fontFamily: 'Mulish',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Button1(
+                    text: 'Start Messaging',
+                    onTab: HomePAge(),
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              "Terms and Conditions",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color.fromARGB(1, 15, 24, 40),
-                fontFamily: 'Mulish',
-                fontSize: 14,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            const Button1(text: 'Start Mes'),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- chat_app/lib/pages/widgets/button.dart
+++ chat_app/lib/pages/widgets/button.dart
@@ -2,29 +2,44 @@ import 'package:flutter/material.dart';
 
 class Button1 extends StatelessWidget {
   final String text;
+  final Widget onTab;
   const Button1({
     super.key,
     required this.text,
+    required this.onTab,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => onTab),
+        );
+      },
+      child: Container(
         height: 52,
         width: 327,
+        alignment: Alignment.center,
         decoration: BoxDecoration(
-          color: const Color.fromARGB(1, 0, 45, 227),
-          borderRadius: BorderRadius.circular(10),
+          color: Colors.blueAccent,
+          // color: const Color.fromARGB(1, 0, 45, 227),
+          borderRadius: BorderRadius.circular(30),
         ),
         child: Center(
-            child: Text(
-          text,
-          style: const TextStyle(
-            color: Color.fromARGB(1, 247, 247, 252),
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
-            fontFamily: 'Poppins',
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+              //  color: Color.fromARGB(1, 247, 247, 252),
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              fontFamily: 'Poppins',
+            ),
           ),
-        )));
+        ),
+      ),
+    );
   }
 }
